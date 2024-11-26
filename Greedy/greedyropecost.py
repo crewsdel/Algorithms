b999fb436d6e1fb6217a599b89d8125069bea42e
crewsdel <124068095+crewsdel@users.noreply.github.com>
2024-11-26 11:33:26 -0500

    Update greedyropecost.py
    Added comments to help with reading

--- Greedy/greedyropecost.py
+++ Greedy/greedyropecost.py
@@ -1,16 +1,16 @@
 import heapq
 def greedyropecost(A):
-    heapq.heapify(A)
-    cost = 0
-    while len(A) > 1:
-        min1 = heapq.heappop(A)
-        min2 = heapq.heappop(A)
-        combined = min1 + min2
-        cost += combined
-        heapq.heappush(A, combined)
+    heapq.heapify(A)  # Turn our input array into a min-heap
+    cost = 0 #Initialize our cost to 0
+    while len(A) > 1: # Check the size of the heap
+        min1 = heapq.heappop(A) # Remove the first minimum element
+        min2 = heapq.heappop(A) # " " second " "
+        combined = min1 + min2 # Combine the two minimum elements together
+        cost += combined # Update the total cost
+        heapq.heappush(A, combined) # Add the new candy and its length back to the heap
     
     print(cost)
     return cost
     
-A = [6, 9, 1, 2]
-greedyropecost(A)
+A = [6, 9, 1, 2] # Change the array values to whatever you like
+greedyropecost(A) # Call the function
