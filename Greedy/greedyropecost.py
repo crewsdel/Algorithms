01004382938005ee20b8b518a85733b41260df07
crewsdel <124068095+crewsdel@users.noreply.github.com>
2024-12-05 08:10:58 -0500

    Update greedyropecost.py

--- Greedy/greedyropecost.py
+++ Greedy/greedyropecost.py
@@ -14,3 +14,12 @@ def greedyropecost(A):
     
 A = [6, 9, 1, 2] # Change the array values to whatever you like
 greedyropecost(A) # Call the function
+
+'''
+Efficiency analysis: 
+This algorithm has a runtime of O(n log n) and occupies O(n) additional space. 
+Each iteration extracts the two minimums (O(log n) because I used a heap) and combines and re-adds the value to the heap (also O(log n)). 
+Therefore, each iteration takes O(log n) + O(log n) = O(log n). 
+The while loop will iterate n – 1 times because we remove 1 item from the heap during each iteration and it terminates when the size of the heap is no longer greater than 1. 
+So, the total runtime cost is O((n – 1) * log n) = O(n log n). The algorithm requires O(n) additional space because we store n elements in our heap at the beginning of our program.
+'''
